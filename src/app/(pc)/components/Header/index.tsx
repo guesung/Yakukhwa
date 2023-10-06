a4fd32e0863baaea45c12c59aac59aa7d1f5e8b5
Kuesung Park <gueit214@naver.com>
2023-10-06 13:08:28 +0900

    refactor : 폴더 구조 수정
    Header 폴더 : Header관련 컴포넌트, Footer 폴더 : Footer 관련 컴포넌트

---
+++ src/app/(pc)/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import LinkListSection from "./LinkListSection";
+
+interface HeaderProps {}
+export default function Header() {
+  return (
+    <header>
+      <LinkListSection />
+    </header>
+  );
+}
