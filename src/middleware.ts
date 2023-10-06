dcb2bf9141d4005f74681f88827e04877ba6de0b
Kuesung Park <gueit214@naver.com>
2023-10-06 09:06:58 +0900

    refactor : middleware 로직 수정
    기존 /m/으로 구분하던 방식은 /m/외의 경우에는 해당이 안되었음

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,15 +16,18 @@ const middleware = async (request: NextRequest) => {
   const isMobileView = !!userAgent!.match(
     /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
   );
-
   const pathname = request.nextUrl.pathname;
-  const isMobileUrl = pathname.includes("/m/");
+  const isMobileUrl = pathname.startsWith("/m");
+
+  /* 모바일 기기에서 PC주소로 접속했을 때 */
   if (isMobileView && !isMobileUrl) {
     const mobileUrl = pathname.replace("/", "/m/");
     return NextResponse.redirect(new URL(mobileUrl, request.nextUrl.origin));
   }
+
+  /* PC 기기에서 모바일 주소로 접속했을 때 */
   if (!isMobileView && isMobileUrl) {
-    const desktopUrl = pathname.replace("/m/", "/");
+    const desktopUrl = pathname.replace("/m", "/");
     return NextResponse.redirect(new URL(desktopUrl, request.nextUrl.origin));
   }
 };
