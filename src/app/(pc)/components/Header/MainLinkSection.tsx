6e9b42fc8d3a4bc6a220efc445cd7b51f891af5b
Kuesung Park <gueit214@naver.com>
2023-10-06 13:58:07 +0900

    feat : 메뉴바 기능 구현

--- src/app/(pc)/components/Header/MainLinkSection.tsx
+++ src/app/(pc)/components/Header/MainLinkSection.tsx
@@ -1,32 +1,26 @@
+"use client";
+
 import { MAINLINKLIST } from "@/constants";
+import { useState } from "react";
 
 export default function MainLinkSection() {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <section className="flex justify-center gap-30">
+    <section
+      className="flex justify-center"
+      onMouseLeave={() => setIsActive(false)}
+    >
       {MAINLINKLIST.map((linkItem, index) => (
-        <LinkItem linkItem={linkItem} key={index} />
+        <div key={index} className="flex flex-col w-120">
+          <div onMouseEnter={() => setIsActive(true)}>{linkItem.mainTitle}</div>
+          <ul>
+            {isActive &&
+              linkItem.subTitle.map((subTitle, i) => (
+                <li key={i}>{subTitle}</li>
+              ))}
+          </ul>
+        </div>
       ))}
     </section>
-  );
-}
-
-interface LinkItemProps {
-  linkItem: {
-    mainTitle: string;
-    subTitle: string[];
-  };
-}
-function LinkItem({ linkItem }: LinkItemProps) {
-  return (
-    <div>
-      <h3>{linkItem.mainTitle}</h3>
-      <ul>
-        {linkItem.subTitle.map((subTitle, index) => (
-          <li key={index}>
-            <a href="#">{subTitle}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
   );
 }
