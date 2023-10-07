98e5ac65902dfbf2d3dc1b11bacb83af1b744ad8
Kuesung Park <gueit214@naver.com>
2023-10-07 09:14:09 +0900

    feat : logo 경로

--- src/app/(pc)/components/Header/LogoSection.tsx
+++ src/app/(pc)/components/Header/LogoSection.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LogoSection() {
   return (
-    <div className="h-100 flex justify-center items-center">
+    <Link className="flex justify-center items-center" href="/">
       <Image src="/images/logo.png" width={200} height={100} alt="로고" />
-    </div>
+    </Link>
   );
 }
