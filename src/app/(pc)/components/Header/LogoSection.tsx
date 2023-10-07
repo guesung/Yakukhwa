9dca8cb81d1f2d2f6dc3ecb3ea26aaa2dff72662
Kuesung Park <gueit214@naver.com>
2023-10-07 15:19:06 +0900

    refactor : 로고 교체

--- src/app/(pc)/components/Header/LogoSection.tsx
+++ src/app/(pc)/components/Header/LogoSection.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function LogoSection() {
   return (
-    <Link className="flex justify-center items-center" href="/">
-      <Image src="/images/logo.png" width={200} height={100} alt="로고" />
+    <Link className="flex justify-center px-20 py-10 items-center" href="/">
+      <Image src="/images/main_logo.png" width={200} height={100} alt="로고" />
     </Link>
   );
 }
