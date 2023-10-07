92f0c7b9111c0f93425f6747981ad0fb88998c24
Kuesung Park <gueit214@naver.com>
2023-10-07 09:14:55 +0900

    feat : 갤러리 페이지

---
+++ src/app/(pc)/chrysanthemum-festival/gallery/[id]/page.tsx
@@ -0,0 +1,29 @@
+import Spacing from "@/components/Spacing";
+import GallerySection from "../components/GallerySection";
+import { dummyData } from "@/constants";
+import PageList from "@/components/PageList";
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+const pageList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default function page({ params: { id } }: PageProps) {
+  return (
+    <div>
+      <h1>국화갤러리</h1>
+      <h2>영암 월출산 국화축제에서</h2>
+      <h2>눈길을 사로잡는 작품들의 시각적 아름다움을 느껴보세요!</h2>
+
+      <Spacing size={20} />
+      <hr />
+      <Spacing size={20} />
+
+      <GallerySection galleryList={dummyData.GALLERYLIST} />
+
+      <PageList maxPage={10} currentPage={+id} />
+    </div>
+  );
+}
