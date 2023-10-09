85aba36839b3f6f78cda6c522a5258a8644aec4b
Kuesung Park <gueit214@naver.com>
2023-10-09 23:41:08 +0900

    refactor : Accodion으로 교체

--- src/app/(pc)/components/Header/SideLinkSection.tsx
+++ src/app/(pc)/components/Header/SideLinkSection.tsx
@@ -19,45 +19,30 @@ export default function SideLinkSection() {
           <Icon id="menu" />
         </SheetTrigger>
         <SheetContent className="flex flex-col items-center py-80 text-center">
-          {MAINLINKLIST.map((linkItem) => (
-            <div key={linkItem.mainTitle.name} className="w-full">
-              <p className="text-title3 py-10">{linkItem.mainTitle.name}</p>
-              <div>
-                {linkItem.subTitle.map((subTitle) => (
-                  <Link
-                    key={subTitle.name}
-                    className="text-title4 py-10 bg-brand-3 h-full w-full block"
-                    href={`/${linkItem.mainTitle.path}/${subTitle.path}`}
-                  >
-                    {subTitle.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          ))}
-
           <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-2">
-              <AccordionTrigger>Is it styled?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It comes with default styles that matches the other
-                components&apos; aesthetic.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>Is it animated?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It's animated by default, but you can disable it if you
-                prefer.
-              </AccordionContent>
-            </AccordionItem>
+            {MAINLINKLIST.map((linkItem) => (
+              <AccordionItem
+                key={linkItem.mainTitle.name}
+                className="w-full"
+                value={linkItem.mainTitle.name}
+              >
+                <AccordionTrigger className="text-title3 py-10">
+                  {linkItem.mainTitle.name}
+                </AccordionTrigger>
+                <div>
+                  {linkItem.subTitle.map((subTitle) => (
+                    <AccordionContent key={subTitle.name}>
+                      <Link
+                        className="text-title4 py-10 bg-brand-3 h-full w-full block"
+                        href={`/${linkItem.mainTitle.path}/${subTitle.path}`}
+                      >
+                        {subTitle.name}
+                      </Link>
+                    </AccordionContent>
+                  ))}
+                </div>
+              </AccordionItem>
+            ))}
           </Accordion>
         </SheetContent>
       </Sheet>
