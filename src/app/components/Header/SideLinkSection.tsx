74ddce15d772aff0a6a333439f1f916032b1327d
Kuesung Park <gueit214@naver.com>
2023-10-10 00:10:04 +0900

    feat : 페이지 이동 시 사이즈 창 꺼지도록 커스텀

--- src/app/components/Header/SideLinkSection.tsx
+++ src/app/components/Header/SideLinkSection.tsx
@@ -10,15 +10,23 @@ import {
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { MAINLINKLIST } from "@/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function SideLinkSection() {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+  useEffect(() => setOpen(false), [pathname]);
   return (
     <section className="absolute right-10 top-10">
-      <Sheet>
-        <SheetTrigger asChild>
+      <Sheet open={open}>
+        <SheetTrigger asChild onClick={() => setOpen(true)}>
           <Icon id="menu" />
         </SheetTrigger>
-        <SheetContent className="flex flex-col items-center py-80 text-center">
+        <SheetContent
+          className="flex flex-col items-center py-80 text-center"
+          setOpen={setOpen}
+        >
           <Accordion type="single" collapsible className="w-full">
             {MAINLINKLIST.map((linkItem) => (
               <AccordionItem
@@ -29,18 +37,16 @@ export default function SideLinkSection() {
                 <AccordionTrigger className="text-title3 py-10 w-full">
                   {linkItem.mainTitle.name}
                 </AccordionTrigger>
-                <div>
-                  {linkItem.subTitle.map((subTitle) => (
-                    <AccordionContent key={subTitle.name}>
-                      <Link
-                        className="text-title4 py-10 bg-brand-3 text-white border-y border-gray-2 h-full w-full block"
-                        href={`/${linkItem.mainTitle.path}/${subTitle.path}`}
-                      >
-                        {subTitle.name}
-                      </Link>
-                    </AccordionContent>
-                  ))}
-                </div>
+                {linkItem.subTitle.map((subTitle) => (
+                  <AccordionContent key={subTitle.name}>
+                    <Link
+                      className="text-title4 py-10 bg-brand-3 text-white border-y border-gray-2 h-full w-full block"
+                      href={`/${linkItem.mainTitle.path}/${subTitle.path}`}
+                    >
+                      {subTitle.name}
+                    </Link>
+                  </AccordionContent>
+                ))}
               </AccordionItem>
             ))}
           </Accordion>
