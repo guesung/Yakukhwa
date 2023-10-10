ff2028f6fb57e2d9828d486cc6401a0a09aff14d
Kuesung Park <gueit214@naver.com>
2023-10-10 20:59:41 +0900

    feat : 서브 메뉴 이미지 추가

--- src/app/chrysanthemum-festival/gallery/layout.tsx
+++ src/app/chrysanthemum-festival/gallery/layout.tsx
@@ -1,16 +1,26 @@
 import Spacing from "@/components/Spacing";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 
 export default function layout({ children }: PropsWithChildren) {
   return (
     <div className="px-50">
       <p className="text-title2">공지사항</p>
-      <p className="text-subtitle2 py-10">
-        영암 월출산 국화축제에서
-        <br />
-        눈길을 사로잡는 작품들의 시각적 아름다움을 느껴보세요!
-      </p>
-      <Spacing size={20} />
+      <div className="relative w-full h-100 flex items-center">
+        <Image
+          src="/images/subtitle_background.jpeg"
+          width={1000}
+          height={300}
+          alt="subtitle"
+        />
+        <p className="text-title2 py-10 absolute text-white top-0 z-10 text-center w-full">
+          영암 월출산 국화축제에서
+          <br />
+          눈길을 사로잡는 작품들의 시각적 아름다움을 느껴보세요!
+        </p>
+      </div>
+
+      <Spacing size={70} />
       <hr />
       <Spacing size={20} />
       {children}
