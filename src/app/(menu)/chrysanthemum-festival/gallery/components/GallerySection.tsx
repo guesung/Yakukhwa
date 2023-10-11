570ccb34eadf140bab738ab739e210cba368242f
Kuesung Park <gueit214@naver.com>
2023-10-11 12:30:34 +0900

    design : padding넣어서 이미지 크기 조정

--- src/app/(menu)/chrysanthemum-festival/gallery/components/GallerySection.tsx
+++ src/app/(menu)/chrysanthemum-festival/gallery/components/GallerySection.tsx
@@ -9,17 +9,23 @@ export default function GallerySection({ galleryList }: GallerySectionProps) {
   return (
     <section className="gap-x-10 gap-y-20 grid grid-cols-2">
       {galleryList.map((gallery) => (
-        <div key={gallery.id}>
+        <article key={gallery.id}>
           <div
             key={gallery.id}
-            className="bg-slate-200 before:block relative before:pb-[100%] max-w-500 flex mx-auto"
+            className="bg-slate-200 before:block relative before:pb-[100%] max-w-500 flex mx-auto overflow-hidden p-50"
           >
             <Image src="/images/frame.png" fill alt="frame" className="z-10" />
 
-            <Image key={gallery.image} src={gallery.image} fill alt={"image"} />
+            <Image
+              key={gallery.image}
+              src={gallery.image}
+              width={400}
+              height={400}
+              alt="image"
+            />
           </div>
           <p className="py-10 text-center">{gallery.title}</p>
-        </div>
+        </article>
       ))}
     </section>
   );
