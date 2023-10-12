8c491a5a01b51ecdccd6d479ede253911d472b7e
Kuesung Park <gueit214@naver.com>
2023-10-12 16:06:27 +0900

    feat : 행사 날짜에 따라 달력 활성화

--- src/app/components/main/ScheduleSection.tsx
+++ src/app/components/main/ScheduleSection.tsx
@@ -1,22 +1,29 @@
 "use client";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { DateController } from "@/utils/DateController";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import Icon from "@/components/Icon";
+import Spacing from "@/components/Spacing";
 import useSwiperController from "@/hooks/useSwipeController";
 import { cn } from "@/utils";
-import Spacing from "@/components/Spacing";
+import { useState } from "react";
+import { festivalDateArray } from "@/constants";
 
 const pageList = [1, 2, 3];
 
 export default function ScheduleSection() {
   const { slideRef, handlePrev, handleNext, hasPrev, hasNext } =
     useSwiperController({ maxPage: pageList.length });
+  const [numberOfDay, setNumberOfDay] = useState(1);
+  const handleDate = (date: string) => {
+    if (!festivalDateArray.includes(date)) return;
+    setNumberOfDay(festivalDateArray.findIndex((it) => it === date) + 1);
+  };
 
   return (
     <section className="px-30 py-20 relative">
@@ -28,16 +35,19 @@ export default function ScheduleSection() {
                 {DateController.getDateList(new Date(), page).map(
                   (date, index) => (
                     <p
-                      key={date}
+                      key={date.date}
                       className={cn(
                         "text-title4 text-gray-6 whitespace-nowrap",
                         {
+                          "cursor-pointer text-black":
+                            festivalDateArray.includes(date.date),
                           "row-start-1": index < 8,
                           "row-start-2": index >= 8,
                         }
                       )}
+                      onClick={() => handleDate(date.date)}
                     >
-                      {date}
+                      {date.date + " " + date.day}
                     </p>
                   )
                 )}
@@ -63,7 +73,7 @@ export default function ScheduleSection() {
       </article>
 
       <article className="h-200 flex flex-col justify-center">
-        <div className="h-60 flex items-center">Day 1</div>
+        <div className="h-60 flex items-center">Day {numberOfDay}</div>
         <div className="flex gap-20 justify-center">
           {[1, 2, 3, 4].map((it) => (
             <div
