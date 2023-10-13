1ea1ce7417e846371cd734d68593b0b8aaece40f
Kuesung Park <gueit214@naver.com>
2023-10-13 21:10:03 +0900

    fix : legacy 코드 제거

--- src/store/firebaseStore.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "@/constants";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseDB = initializeApp(config.firebase);
-export const fireStore = getFirestore(firebaseDB.firebaseApp);
