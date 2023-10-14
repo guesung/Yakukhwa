efab2871f401275a7b8d2c68d63b76e185995e11
Kuesung Park <gueit214@naver.com>
2023-10-15 08:48:23 +0900

    feat : delete 기능

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,5 +1,5 @@
 import CardList from '@/app/(menu)/components/CardList';
-import { DUMMYCARD_LIST, MAINLINKLIST } from '@/constants';
+import { MAINLINKLIST } from '@/constants';
 import { getData } from '@/utils';
 import { getIsAdmin } from '@/utils/getIsAdmin';
 import { getPath } from '@/utils/getPath';
@@ -14,9 +14,9 @@ export default async function CardContainer() {
   const banner = subTitleList?.find((subTitle) => subTitle.path === currentsubTitle)?.banner;
 
   return (
-    <div>
+    <>
       <h1 className="py-30 text-center text-title1">{banner}</h1>
-      <CardList cardList={cardList} />
-    </div>
+      <CardList cardList={cardList} isAdmin={isAdmin} category={currentsubTitle} />
+    </>
   );
 }
