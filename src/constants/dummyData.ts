40a3ebf87485d679f600165719835da538386124
Kuesung Park <gueit214@naver.com>
2023-10-15 09:28:02 +0900

    refactor : 사용하지 않는 더미데이터 제거

--- src/constants/dummyData.ts
+++ src/constants/dummyData.ts
@@ -1,33 +1,4 @@
-import { CardType } from '@/app/type';
-
 export const dummyData = {
-  ANNOUNCEMENT: [
-    {
-      id: '1',
-      title: 'Paid',
-      date: '2023년 10월 7일',
-    },
-    {
-      id: '2',
-      title: 'Pending',
-      date: '2023년 10월 7일',
-    },
-    {
-      id: '3',
-      title: 'Unpaid',
-      date: '2023년 10월 7일',
-    },
-    {
-      id: '4',
-      title: 'Paid',
-      date: '2023년 10월 7일',
-    },
-    {
-      id: '5',
-      title: 'Paid',
-      date: '2023년 10월 7일',
-    },
-  ],
   GALLERYLIST: [
     {
       id: '1',
@@ -370,46 +341,3 @@ type Schedule = {
 type ScehduleList = {
   [key: number]: Schedule[];
 };
-
-export const DUMMYCARD_LIST: CardType[] = [
-  {
-    id: '1',
-    title: '첫 번째 카드',
-    date: '2023-10-14',
-    place: '예시 장소 1',
-    content: '이것은 첫 번째 카드의 내용입니다.',
-    imageUrl: '/images/gallery/1.jpeg',
-  },
-  {
-    id: '2',
-    title: '두 번째 카드',
-    date: '2023-10-15',
-    place: '예시 장소 2',
-    content: '이것은 두 번째 카드의 내용입니다.',
-    imageUrl: '/images/gallery/1.jpeg',
-  },
-  {
-    id: '3',
-    title: '세 번째 카드',
-    date: '2023-10-16',
-    place: '예시 장소 3',
-    content: '이것은 세 번째 카드의 내용입니다.',
-    imageUrl: '/images/gallery/1.jpeg',
-  },
-  {
-    id: '4',
-    title: '세 번째 카드',
-    date: '2023-10-16',
-    place: '예시 장소 3',
-    content: '이것은 세 번째 카드의 내용입니다.',
-    imageUrl: '/images/gallery/1.jpeg',
-  },
-  {
-    id: '5',
-    title: '세 번째 카드',
-    date: '2023-10-16',
-    place: '예시 장소 3',
-    content: '이것은 세 번째 카드의 내용입니다.',
-    imageUrl: '/images/gallery/1.jpeg',
-  },
-];
