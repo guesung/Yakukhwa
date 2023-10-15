78a139ebdaa09e22df8c721b158adff49e4464ad
Kuesung Park <gueit214@naver.com>
2023-10-15 09:56:22 +0900

    design : Spacing 위치 수정

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,11 +1,9 @@
-import Spacing from '@/components/Spacing';
 import HostSection from './HostSection';
 import LinkSection from './LinkSection';
 
 export default function Footer() {
   return (
     <main>
-      <Spacing size={50} />
       <LinkSection />
       <HostSection />
     </main>
