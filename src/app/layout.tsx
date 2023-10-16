2cf55668436dd3e7976282d03cdbcc4545f6ff7e
Kuesung Park <gueit214@naver.com>
2023-10-16 11:00:52 +0900

    refactor : description 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import Analytics from '@/components/Analytics';
 import { config } from '@/config';
 
 const DEFAULT_OG_TITLE = '2023 월출산국화축제';
-const DEFAULT_OG_DESC = '2023 월출산국화축제';
-const DEFAULT_OG_IMAGE = '/images/main_logo.png';
+const DEFAULT_OG_DESC = `깊어가는 가을을 맞아 '국화, 빛으로 피어나다!'라는 주제로 개최되는 이번 축제는 23종, 24만여점의 다양한 국화작품을 전시와 함께 남녀노소 누구나 즐길 수 있는 풍성하고 다채로운 프로그램을 선보일 예정이다. 특히, 축제장 곳곳에 특색있는 야간 포토존 및 조형물 등을 설치하여 야간에도 이색적인 조명 속, 빛으로 물든 아름다운 국화를 감상할 수 있다. 10월 28일부터 11월 12일까지 월출산 기찬랜드에서 개최되는 2023 월출산 국화축제에서는 1억송이 국화가 뿜어내는 짙은 가을의 향기를 느낄 수 있다.`;
+const DEFAULT_OG_IMAGE = '/images/main_image.png';
 
 export const metadata: Metadata = {
   metadataBase: new URL(config.baseUrl),
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
     title: DEFAULT_OG_TITLE,
     description: DEFAULT_OG_DESC,
     images: [DEFAULT_OG_IMAGE],
+    type: 'website',
+    siteName: DEFAULT_OG_TITLE,
+    url: config.baseUrl,
   },
   twitter: {
     title: DEFAULT_OG_TITLE,
