7a4f90882edf03930afda2587cfab5b9bf77f3a1
Kuesung Park <gueit214@naver.com>
2023-10-25 11:22:20 +0900

    design : 경로 수정

--- src/app/(menu)/program/schedule/page.tsx
+++ src/app/(menu)/program/schedule/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function page() {
-  return <Image src="/images/schedule_table.jpeg" width={1400} height={1000} alt="schedule" />;
+  return <Image src="/images/schedule_table.png" width={1400} height={1000} alt="schedule" />;
 }
